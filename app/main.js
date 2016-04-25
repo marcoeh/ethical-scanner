e7194211c19c243c309f53459a45127e2bd0fd62
Marco Ehrenmann <marcoe@me.com>
2016-04-25 14:04:08 +0200

    [Update] Improve state handling and display

--- app/main.js
+++ app/main.js
@@ -12,14 +12,36 @@ $(function() {
         client.subscribe('/questions');
         client.subscribe('/question-nr');
         client.subscribe('/results');
+        client.subscribe('/answer');
     });
 
     client.on('message', function(topic, message) {
         console.log('new message:', topic, message.toString());
 
         if (topic == "/ethic-level") {
-            $('.ethic-level-number').empty();
-            $('.ethic-level-number').append(message.toString());
+
+            var ethicLevel = message.toString();
+
+            switch (ethicLevel) {
+                case "1":
+                    $('.ethic-level-setting__dot-wrapper').removeClass("is-selected");
+                    $('.ethic-level-setting__dot-wrapper--1').addClass("is-selected");
+                    break;
+                case "2":
+                    $('.ethic-level-setting__dot-wrapper').removeClass("is-selected");
+                    $('.ethic-level-setting__dot-wrapper--2').addClass("is-selected");
+                    break;
+                case "3":
+                    $('.ethic-level-setting__dot-wrapper').removeClass("is-selected");
+                    $('.ethic-level-setting__dot-wrapper--3').addClass("is-selected");
+                    break;
+                case "4":
+                    $('.ethic-level-setting__dot-wrapper').removeClass("is-selected");
+                    $('.ethic-level-setting__dot-wrapper--4').addClass("is-selected");
+                    break;
+                default:
+                    console.log("ethicLevel Error");
+            }
         } else if (topic == "/state") {
             $('.state').empty();
 
@@ -27,42 +49,57 @@ $(function() {
 
             switch (state) {
                 case "100":
-                    $('.question').empty();
-                    $('.question').append("Hello there, please scan.");
-                    $('.state-dot').removeClass("is-active");
+                    $('.view--welcome').addClass("is-active");
                     break;
                 case "200":
-                    $('.question').empty();
-                    $('.question').append("You scanned the Product XY. Please set your ethic level.");
+                    $('.view--welcome').removeClass("is-active");
+                    $('.view--welcome').addClass("is-last");
+                    $('.view--level-setting').addClass("is-active");
                     break;
                 case "301":
-                    $('.question').empty();
-                    $('#state-dot-1').addClass("is-active");
+                    $('.view--welcome').removeClass("is-last");
+                    $('.view--level-setting').removeClass("is-active");
+                    $('.view--level-setting').addClass("is-last");
+                    $('.view--question-1').addClass("is-active");
                     break;
                 case "302":
-                    $('#state-dot-2').addClass("is-active");
+                    $('.view--level-setting').removeClass("is-last");
+                    $('.view--question-1').removeClass("is-active");
+                    $('.view--question-1').addClass("is-last");
+                    $('.view--question-2').addClass("is-active");
                     break;
                 case "303":
-                    $('#state-dot-3').addClass("is-active");
+                    $('.view--question-1').removeClass("is-last");
+                    $('.view--question-2').removeClass("is-active");
+                    $('.view--question-2').addClass("is-last");
+                    $('.view--question-3').addClass("is-active");
                     break;
                 case "304":
-                    $('#state-dot-4').addClass("is-active");
+                    $('.view--question-2').removeClass("is-last");
+                    $('.view--question-3').removeClass("is-active");
+                    $('.view--question-3').addClass("is-last");
+                    $('.view--question-4').addClass("is-active");
                     break;
                 case "400":
-                    $('.question').empty();
-                    $('.question').append("Your Results are: TODO");
+                    $('.view--question-3').removeClass("is-last");
+                    $('.view--question-4').removeClass("is-active");
+                    $('.view--question-4').addClass("is-last");
+                    $('.view--results').addClass("is-active");
                     break;
                 case "500":
-                    $('.question').empty();
-                    $('.question').append("Done. Restart?");
+                    $('.view--question-4').removeClass("is-last");
+                    $('.view--question-results').removeClass("is-active");
+                    $('.view--question-results').addClass("is-last");
+                    $('.view--done').addClass("is-active");
                     break;
                 default:
                     $('.state').append("Error Yo");
             }
-
         } else if (topic == "/questions") {
             $('.question').empty();
             $('.question').append(message.toString());
+        } else if (topic == "/results") {
+            $('.results').append(message.toString());
         } else {
             console.log('Nothing to do');
         }
